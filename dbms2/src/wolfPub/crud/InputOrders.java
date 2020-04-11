275d2ab56e1c2a59043e03c7d5358ab46544beb8
Smavinh <smavinh@ncsu.edu>
2020-04-11 02:35:42 -0400

    Bill Order Update

--- dbms2/src/wolfPub/crud/InputOrders.java
+++ dbms2/src/wolfPub/crud/InputOrders.java
@@ -82,6 +82,38 @@ public class InputOrders {
         }
     }
 
+    public static boolean updateOrderpayment(Integer did2, Integer oid2) {
+        try {
+            Connection conn = DbConnection.getConnection();
+            String query = "update ORDERS set TOTALPAYMENT= ? where OrderID = ?";
+
+            PreparedStatement st = conn.prepareStatement(query);
+            String query1 = "select NO_OF_COPIES from CONSISTSOF where OrderID = "+oid2;
+            Integer noc =0;
+            ResultSet rs = st.executeQuery(query1);
+            while (rs.next()) {
+                noc = rs.getInt("NO_OF_COPIES");
+            }
+            String query2 = "select Price from ORDERS where OrderID = "+oid2;
+            float price_val = 0;
+            ResultSet rs2 = st.executeQuery(query2);
+            while (rs2.next()) {
+                price_val = rs2.getFloat("Price");
+            }
+            float val = noc * price_val;
+
+            st.setFloat(1, val);
+            st.setInt(2, oid2);
+
+            st.executeUpdate();
+
+            return true;
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            return false;
+        }
+    }
+
 
 
 
--- dbms2/src/wolfPub/menu/DistributorOperations.java
+++ dbms2/src/wolfPub/menu/DistributorOperations.java
@@ -14,59 +14,74 @@ public class DistributorOperations {
         Float price, shipping_cost, total_pay;
         String placement_date, deadline_date;
         String[] args;
-        System.out.println("1.Input Orders");
-        System.out.println("2.Bill a Distributor for Order");
-        System.out.println("3.Change Balance of Distributor");
-        System.out.println("4.exit");
-        String input = reader.readLine();
-        switch (Integer.parseInt(input)) {
-            case 1:
-                System.out.println("Enter | separated Integer DID");
-                args = reader.readLine().split("[|]");
-                did = Integer.valueOf(Integer.parseInt(args[0]));
-                System.out.println("Enter | separated Integer OrderID, String Deadline_Date, String Placement_Date,Float Shipping Cost,float price,float total_payment");
-                args = reader.readLine().split("[|]");
-                orderid = Integer.valueOf(Integer.parseInt(args[0]));
-                deadline_date = args[1];
-                placement_date = args[2];
-                shipping_cost = Float.valueOf(Float.parseFloat(args[3]));
-                price = Float.valueOf(Float.parseFloat(args[4]));
-                total_pay = Float.valueOf(Float.parseFloat(args[5]));
-                System.out.println("Enter | separated PUBLICATION ID AND NUMBER OF COPIES");
-                args = reader.readLine().split("[|]");
-                Integer pid = Integer.valueOf(Integer.parseInt(args[0]));
-                Integer noc = Integer.valueOf(Integer.parseInt(args[1]));
+        boolean exit_val = true;
+        while(exit_val) {
+            System.out.println("1.Input Orders");
+            System.out.println("2.Bill a Distributor for Order");
+            System.out.println("3.Change Balance of Distributor");
+            System.out.println("4.exit");
+            String input = reader.readLine();
 
 
+            switch (Integer.parseInt(input)) {
+                case 1:
+                    System.out.println("Enter | separated Integer DID");
+                    args = reader.readLine().split("[|]");
+                    did = Integer.valueOf(Integer.parseInt(args[0]));
+                    System.out.println("Enter | separated Integer OrderID, String Deadline_Date, String Placement_Date,Float Shipping Cost,float price,float total_payment");
+                    args = reader.readLine().split("[|]");
+                    orderid = Integer.valueOf(Integer.parseInt(args[0]));
+                    deadline_date = args[1];
+                    placement_date = args[2];
+                    shipping_cost = Float.valueOf(Float.parseFloat(args[3]));
+                    price = Float.valueOf(Float.parseFloat(args[4]));
+                    total_pay = Float.valueOf(Float.parseFloat(args[5]));
+                    System.out.println("Enter | separated PUBLICATION ID AND NUMBER OF COPIES");
+                    args = reader.readLine().split("[|]");
+                    Integer pid = Integer.valueOf(Integer.parseInt(args[0]));
+                    Integer noc = Integer.valueOf(Integer.parseInt(args[1]));
 
 
-                InputOrders.insertOrder(orderid,deadline_date,placement_date,shipping_cost,price,total_pay);
+                    InputOrders.insertOrder(orderid, deadline_date, placement_date, shipping_cost, price, total_pay);
 
-                InputOrders.insertPlaces(orderid,did);
-                InputOrders.insertConsistsOf(orderid,pid,noc);
-                return;
+                    InputOrders.insertPlaces(orderid, did);
+                    InputOrders.insertConsistsOf(orderid, pid, noc);
+                    return;
 
-            case 2:
+                case 2:
 
-                // insertion required
+                    // insertion required
+
+                    System.out.println("Enter | separated Integer DID and Order ID");
+                    args = reader.readLine().split("[|]");
+                    Integer did2 = Integer.valueOf(Integer.parseInt(args[0]));
+                    Integer oid2 = Integer.valueOf(Integer.parseInt(args[1]));
+                    if (InputOrders.updateOrderpayment(did2, oid2)) {
+                        System.out.println("Operation Successful");
+                    } else {
+                        System.out.println("Operation Failed");
+                    }
+                    return;
+
+                case 3:
+                    System.out.println("Enter | separated Integer DID and Balance for updation");
+                    args = reader.readLine().split("[|]");
+                    Integer did3 = Integer.valueOf(Integer.parseInt(args[0]));
+                    Float balance = Float.valueOf(Float.parseFloat(args[1]));
+                    if (DistributorCRUD.updateDistributorBalance(did3, balance)) {
+                        System.out.println("Operation Successful");
+                    } else {
+                        System.out.println("Operation Failed");
+                    }
+                    return;
+                case 4:
+                    exit_val = false;
+                    return;
 
 
+            }
 
-
-
-            case 3:
-                System.out.println("Enter | separated Integer DID and Balance for updation");
-                args = reader.readLine().split("[|]");
-                Integer did1 = Integer.valueOf(Integer.parseInt(args[0]));
-                Float balance = Float.valueOf(Float.parseFloat(args[1]));
-                if(DistributorCRUD.updateDistributorBalance(did1,balance)){
-                    System.out.println("Operation Successful");
-                } else {
-                    System.out.println("Operation Failed");
-                }
-
-
-                }
+        }
 
 
         }
